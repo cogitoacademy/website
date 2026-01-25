fcc57165ca3c286f007c05eeef6d1af3092ffe1a
Andreandhiki <andreandhikiriyantap@mail.ugm.ac.id>
2026-01-25 14:48:51 +0700

    fix: default theme to light and faq

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "./ui/sonner";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
       <MotionProvider>
         {children}
         <Toaster richColors />
